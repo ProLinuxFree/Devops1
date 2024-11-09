74a97989458c2ac804569283917083981088f734
madhuri <madhuriniraj1606@gmail.com>
2024-11-08 19:24:57 -0600

    created demo.py

---
+++ demo.py
@@ -0,0 +1,10 @@
+#Ask the user for input of a character (vowel or consonant)
+ch = input(“Enter a character: “)
+
+if(ch == ‘a’ or ch == ‘e’ or ch == ‘i’ or ch == ‘o’ or ch == ‘u’):
+
+  print(“The given character”, ch, “is a vowel.”) //if the character is found to be vowel
+
+else:
+
+  Print (“Given character”, ch, “is a consonant.”) //if the given character is consonant.
